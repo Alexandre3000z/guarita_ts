32dbe8f143a871fa1ef5ff820f410488dca511aa
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-22 22:58:39 -0300

    Declarando cliente

--- socket/index.ts
+++ socket/index.ts
@@ -2,17 +2,18 @@ import { createServer, Socket } from "net";
 
 export const server = createServer((client: Socket) => {
   console.log("Cliente conectado:", client.remoteAddress, client.remotePort);
-
+  const clienteId = `${client.remoteAddress}:${client.remotePort}`;
+  
   client.on("data", (data) => {
     console.log("Recebido do cliente:", data.toString());
     // aqui você processa e, se for bridge, envia para outro socket…
   });
 
   client.on("end", () => {
-    console.log("Cliente desconectou");
+    console.log(`Cliente desconectou: ${clienteId}`);
   });
 
   client.on("error", (err) => {
-    console.error("Erro no socket do cliente:", err);
+    console.error(`Erro no socket ${clienteId}:`, err);
   });
 });
