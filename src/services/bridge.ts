df27babf67f22eabda0acd07f7b2afebefb9e785
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-27 22:06:16 -0300

    Tirando Console log da bridge

--- src/services/bridge.ts
+++ src/services/bridge.ts
@@ -11,19 +11,19 @@ export function createBridge(a: Socket, b: Socket) {
 
   a.on("data", (chunk) => {
     // log no console o que o app (Nice) está enviando para a guarita
-    console.log(`[→] ${idA} → ${idB} | ${chunk.length} bytes`);
+    // console.log(`[→] ${idA} → ${idB} | ${chunk.length} bytes`);
     // Se quiser ver como texto:
-    console.log(`    Texto: ${chunk.toString("utf8")}`);
+    // console.log(`    Texto: ${chunk.toString("utf8")}`);
     // Ou, para bytes brutos (hex):
-    console.log(`    Hex: ${chunk.toString("hex")}`);
+    // console.log(`    Hex: ${chunk.toString("hex")}`);
     b.write(chunk);
   });
 
   b.on("data", (chunk) => {
     // log no console o que a guarita está enviando para o app
-    console.log(`[←] ${idB} → ${idA} | ${chunk.length} bytes`);
-    console.log(`    Texto: ${chunk.toString("utf8")}`);
-    console.log(`    Hex: ${chunk.toString("hex")}`);
+    // console.log(`[←] ${idB} → ${idA} | ${chunk.length} bytes`);
+    // console.log(`    Texto: ${chunk.toString("utf8")}`);
+    // console.log(`    Hex: ${chunk.toString("hex")}`);
     a.write(chunk);
   });
 
